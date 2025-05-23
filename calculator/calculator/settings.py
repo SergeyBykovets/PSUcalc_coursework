3e1220d83306d7d40edc358cdb22d86da5ee9dc8
Olexiy Fariga <farigaolexiy@gmail.com>
2025-05-23 03:01:37 +0300

    Update settings.py Changed setting.py to avoid warning "?: (staticfiles.W004) The directory '??\calculator\static' in the STATICFILES_DIRS setting does not exist." Add constant STATIC_ROOT to setting.py

--- calculator/calculator/settings.py
+++ calculator/calculator/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# old STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "main" / "static"]
+
+#add new constant STATIC_ROOT: dir for collect the static files
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
