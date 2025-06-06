eb032360a30129eb86cc14434519f7a975775d28
SergeyBykovets <bykovet@gmail.com>
2025-06-06 11:05:50 +0300

    final

--- calculator/staticfiles/js/main.js
+++ calculator/staticfiles/js/main.js
@@ -3,9 +3,7 @@ import PSCalculatorView from './PSCalculatorView.js';
 import PSCalculatorController from './PSCalculatorController.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Initializing MVC...');
     const model = new PSCalculatorModel();
     const view = new PSCalculatorView();
     const controller = new PSCalculatorController(model, view);
-    console.log('MVC Initialized', { model, view, controller });
 });
